b6838f94efab00966eb7d3e8069eece6f846a714
Steve Henty <stevehenty@gmail.com>
2016-03-01 15:58:53 +0100

    version bump

--- apidemo2.php
+++ apidemo2.php
@@ -3,12 +3,12 @@
 Plugin Name: Gravity Forms Web API Demo 2 Add-On
 Plugin URI: http://www.gravityforms.com
 Description: Demonstrates signature authentication for external clients of the Gravity Forms API.
-Version: 1.0
+Version: 1.0.1
 Author: stevehenty
 Author URI: http://www.stevenhenty.com
 
 ------------------------------------------------------------------------
-Copyright 2015 Steven Henty
+Copyright 2015-2016 Steven Henty
 
 This program is free software; you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
@@ -28,13 +28,13 @@ Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
 //------------------------------------------
 
 
-define( 'GF_WEB_API_DEMO_2_VERSION', '1.0' );
+define( 'GF_WEB_API_DEMO_2_VERSION', '1.0.1' );
 
 add_action( 'gform_loaded', array( 'GF_Web_Api_Demo_2_Bootstrap', 'load' ), 5 );
 
 class GF_Web_Api_Demo_2_Bootstrap {
 
-	public static function load(){
+	public static function load() {
 
 		if ( ! method_exists( 'GFForms', 'include_addon_framework' ) ) {
 			return;
@@ -44,9 +44,8 @@ class GF_Web_Api_Demo_2_Bootstrap {
 
 		GFAddOn::register( 'GF_Web_Api_Demo_2' );
 	}
-
 }
 
-function gf_web_api_demo_2(){
+function gf_web_api_demo_2() {
 	return GF_Web_Api_Demo_2::get_instance();
 }
